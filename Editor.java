d6bd49b31cb5fc08503e909a217b9984ad711ff2
FlamingSeahorse <flamingseahorsey@gmail.com>
2016-12-15 23:28:26 +0200

    added next and prev button

--- Editor.java
+++ Editor.java
@@ -6,7 +6,9 @@ import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
 public class Editor extends JPanel {
-    private BufferedImage img = ResourceLoader.loadImage("animate2.png");
+    private BufferedImage img = ResourceLoader.loadImage("spritesheet1.png");
+    private BufferedImage imgnext = ResourceLoader.loadImage("btn_next.png");
+    private BufferedImage imgprevious = ResourceLoader.loadImage("btn_previous.png");
     private BufferedImage cursor;
     private int sceneWidth;
     private int sceneHeight;
@@ -26,15 +28,16 @@ public class Editor extends JPanel {
         grid = new Tile[rows][cols];
         this.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent arg0) {
-                if(mouseLocY > rows * blockSize){ //Clicked inside Pallette
-                    pallettePicked = ((mouseLocY - rows * blockSize)/blockSize)*10 + mouseLocX/blockSize;
-                    cursor = img.getSubimage((mouseLocX/blockSize)*blockSize, ((mouseLocY-rows * blockSize)/blockSize)*blockSize, blockSize, blockSize);
-                }
-                else{ //Clicked inside Scene
+
+                if(mouseLocY > rows * blockSize && mouseLocX < 15 * blockSize){ //Clicked inside Pallette
+                        pallettePicked = ((mouseLocY - rows * blockSize) / blockSize) * 10 + mouseLocX / blockSize;
+                        cursor = img.getSubimage((mouseLocX / blockSize) * blockSize, ((mouseLocY - rows * blockSize) / blockSize) * blockSize, blockSize, blockSize);
+                    }
+                else if(mouseLocY < rows * blockSize && mouseLocX < 15 * blockSize){ //Clicked inside Scene
                     if(pallettePicked!= -1){
                         grid[mouseLocY/blockSize][mouseLocX/blockSize] = new Tile(cursor, (mouseLocX/blockSize)*blockSize, (mouseLocY/blockSize)*blockSize,pallettePicked);
                     }
-                }
+                }else checkButtonPressed();
 
             }
     });
@@ -58,11 +61,26 @@ public class Editor extends JPanel {
                 if(grid[j][i] != null)grid[j][i].paint(g);
             }
         }
+        //Draw Buttons
+        g.drawImage(imgnext, 15*blockSize,blockSize*rows ,null);
+        g.drawImage(imgprevious, 15*blockSize,blockSize*rows + 42 ,null);
     }
 
     public void move(JFrame frame){
         setMouseLocX(MouseInfo.getPointerInfo().getLocation().x -frame.getLocation().x - 8);
         setMouseLocY(MouseInfo.getPointerInfo().getLocation().y -frame.getLocation().y - 32);
+    }
+    public void checkButtonPressed(){
+        if(mouseLocY > rows * blockSize && mouseLocX > 15 * blockSize){ //Check if its in the button area
+            if(mouseLocY < rows * blockSize + 42)pressedNext();
+            else pressedPrevious();
+        }
+    }
+    public void pressedNext(){
+        img = ResourceLoader.loadImage("spritesheet2.png");
+    }
+    public void pressedPrevious(){
+        img = ResourceLoader.loadImage("spritesheet1.png");
     }
     public void setMouseLocX(int mouseX){
         mouseLocX = mouseX;
