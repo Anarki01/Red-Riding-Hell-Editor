b0fa181e2cb3c2473094bffa79d73ff8cd175b7f
FlamingSeahorse <flamingseahorsey@gmail.com>
2016-12-16 12:36:29 +0200

    can shift scene left and right

--- Editor.java
+++ Editor.java
@@ -21,6 +21,9 @@ public class Editor extends JPanel {
     private Tile[][] grid;
     private int pallettePicked = -1;
 
+    private int xShift = 0;
+    private int yShift = 0;
+
     private int cols, rows;
     private int mouseLocX, mouseLocY;
 
@@ -30,7 +33,7 @@ public class Editor extends JPanel {
         this.blockSize = blockSize;
         cols = sceneWidth / blockSize;
         rows = sceneHeight / blockSize;
-        grid = new Tile[rows][cols];
+        grid = new Tile[10][30];
         this.addMouseListener(new MouseAdapter() {
             public void mousePressed(MouseEvent arg0) {
 
@@ -38,9 +41,9 @@ public class Editor extends JPanel {
                         pallettePicked = ((mouseLocY - rows * blockSize) / blockSize) * 10 + mouseLocX / blockSize;
                         cursor = img.getSubimage((mouseLocX / blockSize) * blockSize, ((mouseLocY - rows * blockSize) / blockSize) * blockSize, blockSize, blockSize);
                     }
-                else if(mouseLocY < rows * blockSize && mouseLocX < 15 * blockSize){ //Clicked inside Scene
+                else if(mouseLocY < rows * blockSize && mouseLocX < 16 * blockSize){ //Clicked inside Scene
                     if(pallettePicked!= -1){
-                        grid[mouseLocY/blockSize][mouseLocX/blockSize] = new Tile(cursor, (mouseLocX/blockSize)*blockSize, (mouseLocY/blockSize)*blockSize,pallettePicked);
+                        grid[mouseLocY/blockSize][mouseLocX/blockSize+xShift] = new Tile(cursor, (mouseLocX/blockSize)*blockSize, (mouseLocY/blockSize)*blockSize,pallettePicked);
                     }
                 }else checkButtonPressed();
 
@@ -52,14 +55,15 @@ public class Editor extends JPanel {
         g.setColor(Color.BLACK);
         for (int j = 0; j < rows + 1; j++) {
             for (int i = 0; i < cols+ 1; i++) {
-                g.drawLine(i * blockSize, 0, i * blockSize, sceneHeight);
+                g.drawLine(i * blockSize, 0, i * blockSize, blockSize * rows);
             }
-            g.drawLine(0, j * blockSize, sceneWidth, j * blockSize);
+            g.drawLine(0, j * blockSize, blockSize * cols, j * blockSize);
         }
         //Draw Buttons
         g.drawImage(imgnext, 15*blockSize,blockSize*rows ,null);
         g.drawImage(imgprevious, 15*blockSize,blockSize*rows + 42 ,null);
         g.drawImage(imgsave, 15*blockSize,blockSize*rows + 84 ,null);
+        g.drawImage(imgnext, 15*blockSize,blockSize*rows + 126 ,null);
         //Paint spritesheet
         g.drawImage(img, 0, rows * blockSize, null);
         //Paint cursor
@@ -67,8 +71,14 @@ public class Editor extends JPanel {
         //Draw Tiles
         for(int j = 0; j < rows; j++){
             for(int i = 0; i < cols; i++) {
-                if(grid[j][i] != null)grid[j][i].paint(g);
+                if(grid[j][i+xShift] != null)grid[j][i+xShift].paint(g);
             }
+        }
+        //Draw tile numbers
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", 0, 10));
+        for(int i = 0; i < cols; i++) {
+            g.drawString("" + (i+xShift), i*blockSize, 10);
         }
     }
 
@@ -81,8 +91,12 @@ public class Editor extends JPanel {
             if(mouseLocY < rows * blockSize + 42)pressedNext();
             else if(mouseLocY < rows * blockSize + 84){
                 pressedPrevious();
-            }else{
+            }else if(mouseLocY < rows * blockSize + 126){
                 readToFile();
+            }else if(mouseLocY < rows * blockSize + 168){
+                if(xShift < 14)shiftLeft();
+            }else{
+                if(xShift > 0)shiftRight();
             }
         }
     }
@@ -104,6 +118,24 @@ public class Editor extends JPanel {
             writer.close();
         }catch(Exception e){
             System.out.println("Failed to Open.");
+        }
+    }
+    public void shiftLeft(){
+        xShift++;
+        for(int j = 0; j < 10; j++){
+            for(int i = 0; i < 30; i++){
+                if(grid[j][i] != null)grid[j][i].setX(grid[j][i].getX() - blockSize);
+                if(grid[j][i] != null)System.out.println(i + " : " + j + " : " + grid[j][i].getID());
+            }
+        }
+    }
+    public void shiftRight(){
+        xShift--;
+        for(int j = 0; j < 10; j++){
+            for(int i = 0; i < 30; i++){
+                if(grid[j][i] != null)grid[j][i].setX(grid[j][i].getX() + blockSize);
+                if(grid[j][i] != null)System.out.println(i + " : " + j + " : " + grid[j][i].getID());
+            }
         }
     }
     public void setMouseLocX(int mouseX){
--- Tile.java
+++ Tile.java
@@ -26,4 +26,11 @@ public class Tile extends JPanel {
     public int getY(){
         return y;
     }
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+
 }
